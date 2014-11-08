d3fbba6ef149dcfc9d9a6a567b8eb589df61ce61
Rushi Shah <2016rshah@gmail.com>
2014-11-08 14:13:27 -0500

    Got rid of video, too laggy

--- client/client.js
+++ client/client.js
@@ -1,23 +1,35 @@
 if (Meteor.isClient) {
-
     $(document).ready(function() {
         $('#fullpage').fullpage({
-            anchors:['landing', 'slide1', 'slide2', 'slide3'],
+            anchors: ['landing', 'slide1', 'slide2', 'slide3'],
             css3: true,
             sectionsColor: ['000', '000', '#4BBFC3']
         });
+        //$('#myVideo').get(0).play();
     });
-
     Template.header.helpers({
-    	greeting:function(){
-    		greetings = ["Welcome!", "Bienvenue!", "Willkommen!", "Bienvenida!", "Welkom!", "Benvenuto!"]
-    		var greeting = greetings[Math.floor(Math.random()*greetings.length)];
-    		return greeting;
-    	}//comma here
+        greeting: function() {
+            greetings = ["Welcome!", "Bienvenue!", "Willkommen!", "Bienvenida!", "Welkom!", "Benvenuto!"]
+            var greeting = greetings[Math.floor(Math.random() * greetings.length)];
+            return greeting;
+        } //comma here
     });
-
-    UI.registerHelper("isMobile", function(){
-	    var index = navigator.appVersion.indexOf("Mobile");
-	    return (index > -1);
-	})
+    UI.registerHelper("isMobile", function() {
+        var index = navigator.appVersion.indexOf("Mobile");
+        return (index > -1);
+    })
+    // Template.landing.events({
+    //     'click #video-container': function () {
+    //         if($("#myVideo").get(0).paused){
+    //             $('#myVideo').get(0).play();
+    //         }
+    //         else{
+    //             $("#myVideo").get(0).pause();
+    //         }
+    //     }
+    // });
+    isMobile = function(){
+        var index = navigator.appVersion.indexOf("Mobile");
+        return (index > -1);
+    }
 }
